eca6e71b99db645efc6bd4b2bfac350145359627
4aykovski <kan89kan@gmail.com>
2023-12-26 13:27:55 +0300

    логирование, запуск через консоль

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,23 +1,41 @@
 package main
 
 import (
+	"flag"
 	"log"
 	"net/http"
+	"os"
 	"path/filepath"
 )
 
 func main() {
+	// настройка конфига через консоль
+	addr := flag.String("addr", ":8080", "Сетевой адрес HTTP")
+	flag.Parse()
+
+	// кастомное логирование
+	infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
+	errLog := log.New(os.Stderr, "ERROR\t", log.Ldate|log.Ltime|log.Llongfile)
+
+	// создание мультиплексора и регистрация обработчиков
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", home)
 	mux.HandleFunc("/snippet", showSnippet)
 	mux.HandleFunc("/snippet/create", createSnippet)
 
+	// регистрация обработчика файловой системы
 	fileServer := http.FileServer(&neuteredFileSystem{http.Dir("./ui/static")})
 	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
 
-	log.Println("Запуск веб-сервера на http://localhost:8080")
-	err := http.ListenAndServe(":8080", mux)
-	log.Fatal(err)
+	server := http.Server{
+		Addr:     *addr,
+		ErrorLog: errLog,
+		Handler:  mux,
+	}
+	infoLog.Println(infoLog.Flags())
+	infoLog.Printf("Запуск сервера на порту %s", *addr)
+	err := server.ListenAndServe()
+	errLog.Fatal(err)
 }
 
 type neuteredFileSystem struct {
